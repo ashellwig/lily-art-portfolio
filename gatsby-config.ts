5bc1138e57cd53dbf893a0fef163c0e4355de946
Ash Hellwig <ahellwig.dev@gmail.com>
2024-12-03 23:49:57 -0700

    Description
    Signed-off-by: Ash Hellwig <ahellwig.dev@gmail.com>

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -12,7 +12,7 @@ const config: GatsbyConfig = {
     siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
     siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
     siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Portfolio website for Lily Thomas's (peachpuke) art and makeup art.`,
+    siteDescription: `Portfolio website for Lily Ellen Thomas's (peachpuke) makeup art.`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
     author: `@lekoarts_de`,
@@ -29,7 +29,7 @@ const config: GatsbyConfig = {
       options: {
         name: `Cara - @lekoarts/gatsby-theme-cara`,
         short_name: `Cara`,
-        description: `Portfolio website for Lily Thomas's (peachpuke) art and makeup art.`,
+        description: `Portfolio website for Lily Ellen Thomas's (peachpuke) makeup art.`,
         start_url: `/`,
         background_color: `#141821`,
         // This will impact how browsers show your PWA/website
